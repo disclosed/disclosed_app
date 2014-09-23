896319d5ac40c23699cbd3d0015ef986906b4b67
ishestak <shestak.irina@gmail.com>
2014-09-22 22:09:48 -0700

    filterrific in Contracts Controller

--- app/controllers/api/contracts_controller.rb
+++ app/controllers/api/contracts_controller.rb
@@ -4,6 +4,15 @@ class Api::ContractsController < ApplicationController
 
   def index
     @contracts = Contract.all.limit(25)
+    @filterrific = Filterific.new(
+      Contract, params[:filterrific] || session[:filterrific_contracts] 
+    )
+    @filterrific.select_options = {
+      sorted_by: Contract.options_for_sorted_by
+    }
+    @contracts = Contract.filterrific_find(@filterrific)
+    session[filterrific_contracts] = @filterrific.to_hash
+    
     respond_with(@contracts)
   end
 
@@ -20,5 +29,7 @@ class Api::ContractsController < ApplicationController
     @contracts = Contract.filter(params[:query])  # A Contract class method (self.filter) needs to be created in the Contract model in order for this to be implemented - use scopes?
     respond_with(@contracts)
   end
-
+  
+  def filtered_results
+  end
 end
