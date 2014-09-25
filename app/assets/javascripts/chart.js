a61c0972869fa21217c008f9bd523dee52fd263a
passivoice <cooke.ka@gmail.com>
2014-09-24 18:12:04 -0700

    Enabled turbolinks and gon to pass chart data from backend to JS in view

--- app/assets/javascripts/chart.js
+++ app/assets/javascripts/chart.js
@@ -3,12 +3,8 @@ $(function(){
     bindto: '#chart',
     data: {
       // all the input data for the chart belongs to the 'column' array
-      columns: [
-//        ['x', date, date, date, date]
-        ['data1', 30, 200, 100, 400, 150, 250],
-        ['data2', 50, 20, 10, 40, 15, 25]
-      ]
-    },
+      columns: gon.chart_data
+    }
     // One of the columns is specified as an x-axis. 
   //  axis: {
     //  x: {
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
+    params[:vendor] ||= "Bonanza"
     @agencies = Agency.all
+    gon.chart_data = [
+      ['data1', 30, 100, 100, 400, 150, 250],
+      ['data2', 50, 20, 10, 40, 15, 25]
+    ]
   end
+
 end
