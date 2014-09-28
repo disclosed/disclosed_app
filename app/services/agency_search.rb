c8e9a28ce620d3fd932f5adbc0e58608b98f9ae0
passivoice <cooke.ka@gmail.com>
2014-09-28 01:24:20 -0700

    refactored AgencySearch to resemble VendorSearch, and fixed ContractSearch to remove empty string params from multi-vendor searchbox

--- app/services/agency_search.rb
+++ app/services/agency_search.rb
@@ -6,37 +6,45 @@ class AgencySearch
   end
   
   def search
-    find_aggregate_dates
-    find_aggregate_values   
+    @agencies.each do |agency|
+      agency_match = match(agency)
+      matched_name = Agency.find(agency).name
+      @chart_data << format_date_results(agency_match)
+      @chart_data << format_value_results(agency_match, matched_name)
+    end
     @chart_data
   end
 
-  def find_aggregate_dates
-    @agencies.each do |agency|
-      agency_results = Contract.spending_per_agency(agency)
-      @chart_data << format_date_results(agency_results)
-    end
+  def match(agency)
+    Contract.spending_per_agency(agency)
   end
 
-  def find_aggregate_values
-    @agencies.each do |agency|
-      agency_results = Contract.spending_per_agency(agency)
-      matched_name = Agency.find(agency).name
-      @chart_data << format_value_results(agency_results, matched_name)
-    end
-  end
+  # def find_aggregate_dates
+  #   @agencies.each do |agency|
+  #     agency_results = Contract.spending_per_agency(agency)
+  #     @chart_data << format_date_results(agency_results)
+  #   end
+  # end
 
-  def format_date_results(agency_results)
+  # def find_aggregate_values
+  #   @agencies.each do |agency|
+  #     agency_results = Contract.spending_per_agency(agency)
+  #     matched_name = Agency.find(agency).name
+  #     @chart_data << format_value_results(agency_results, matched_name)
+  #   end
+  # end
+
+  def format_date_results(agency_match)
     dates = []
-    agency_results.each do |agency_spending_for_year|
+    agency_match.each do |agency_spending_for_year|
       dates << "#{agency_spending_for_year.year.round(0)}-01-01"
     end
     dates.unshift("Date")
   end
 
-  def format_value_results(agency_results, matched_name)
+  def format_value_results(agency_match, matched_name)
     agency_sum_values= []
-    agency_results.each do |agency_spending_for_year|
+    agency_match.each do |agency_spending_for_year|
       agency_sum_values << agency_spending_for_year.total
     end
     agency_sum_values.unshift(matched_name)
