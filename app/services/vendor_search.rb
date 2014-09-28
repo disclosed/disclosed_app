4c70a4266350151ed385fc90502e71b10f82e915
passivoice <cooke.ka@gmail.com>
2014-09-27 23:57:13 -0700

    refactored VendorSearch

--- app/services/vendor_search.rb
+++ app/services/vendor_search.rb
@@ -7,37 +7,30 @@ class VendorSearch
   end
 
   def search
-    find_aggregate_dates
-    find_aggregate_values
+    @vendors.each do |vendor|
+      vendor_match = match(vendor)
+      matched_name = Contract.vendor_name(vendor).first.vendor_name
+      @chart_data << format_date_results(vendor_match)
+      @chart_data << format_value_results(vendor_match, matched_name)
+    end
     @chart_data
   end
 
-  def find_aggregate_dates
-    @vendors.each do |vendor|
-      vendor_results = Contract.spending_per_vendor(vendor)
-      @chart_data << format_date_results(vendor_results)
-    end
+  def match(vendor)
+    Contract.spending_per_vendor(vendor)
   end
 
-  def find_aggregate_values
-    @vendors.each do |vendor|
-      vendor_results = Contract.spending_per_vendor(vendor)
-      matched_name = Contract.vendor_name(vendor).first.vendor_name
-      @chart_data << format_value_results(vendor_results, matched_name)
-    end
-  end
-
-  def format_date_results(vendor_results)
+  def format_date_results(vendor_match)
     dates = []
-    vendor_results.each do |vendor_spending_for_year|
+    vendor_match.each do |vendor_spending_for_year|
       dates << "#{vendor_spending_for_year.year.round(0)}-01-01"
     end
     dates.unshift("Date")
   end
 
-  def format_value_results(vendor_results, matched_name)
+  def format_value_results(vendor_match, matched_name)
     vendor_sum_values =[]
-    vendor_results.each do |vendor_spending_for_year|
+    vendor_match.each do |vendor_spending_for_year|
       vendor_sum_values << vendor_spending_for_year.total
     end
     vendor_sum_values.unshift(matched_name)
