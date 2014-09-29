739c06ff4df456d0627c061bd482bf6fcb5da8fb
ishestak <shestak.irina@gmail.com>
2014-09-28 21:34:09 -0700

    select boxes aligned and in proper spot

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,12 +1,12 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
-//$(document).on("page:change", function(){
+$(document).on("page:change", function(){
   
-  //$(".chosen-select").chosen();
+  $(".chosen-select").chosen();
   
-  //$('.search-form').on('click', function(event){
-    //event.preventDefault();
-   // $(this).next('.hidden-form').show();
- // });
-//});
+  $('.search-form').on('click', function(event){
+    event.preventDefault();
+    $(this).next('.hidden-form').show();
+  });
+});
