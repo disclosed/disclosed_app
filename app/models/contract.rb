8d746ebb33a94ee4e6a31730758ba0cf8c49b598
passivoice <cooke.ka@gmail.com>
2014-09-30 17:15:39 -0700

    Fixed csv generation

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -79,10 +79,10 @@ class Contract < ActiveRecord::Base
     contract
   end
   
-  def self.to_csv
+  def self.to_csv(contracts)
     CSV.generate do |csv|
       csv << column_names
-      all.each do |contract|
+      contracts.each do |contract|
         csv << contract.attributes.values_at(*column_names)
       end
     end
