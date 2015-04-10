e5a3dbf3ef9df563fed2d5ce52d44a2062b7a04f
Monica Olinescu <monica.olinescu@gmail.com>
2015-04-10 11:02:41 -0700

    Deployment: Fifteenth time's a charm

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,9 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
-set :linked_files,    %w(config/database.yml config/application.yml config/secrets.yml)
+set :linked_files,    %w(config/database.yml config/application.yml)
 set :linked_dirs,     %w(tmp/pids tmp/sockets log)
+
+set :default_env, {
+  'RACK_ENV' => 'production'
+}
