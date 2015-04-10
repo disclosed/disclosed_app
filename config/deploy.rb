210691011d1c42ed8795a2b1a4ef4113340a77af
Monica Olinescu <monica.olinescu@gmail.com>
2015-04-10 10:57:05 -0700

    Deployment: Please work....

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
 # config valid only for Capistrano 3.1
 lock '3.1.0'
 
+set :linked_files,    %w(config/database.yml config/application.yml config/secrets.yml)
+set :linked_dirs,     %w(tmp/pids tmp/sockets log)
