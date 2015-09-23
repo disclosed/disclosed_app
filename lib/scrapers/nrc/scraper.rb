02faa327b234b24374c3fdae5e64f49c0bab8106
Monica Olinescu <monica.olinescu@gmail.com>
2015-09-24 01:45:44 +0200

    Scraper for nrc

---
+++ lib/scrapers/nrc/scraper.rb
@@ -0,0 +1,56 @@
+class Scrapers::Nrc::Scraper < Scrapers::ContractScraper
+  BASE_URL = "http://www.nrc-cnrc.gc.ca"
+  # Scrape contract information from a page.
+  #
+  # url::
+  #   A URL for a contract details page.
+  #   Ex: "http://www.pc.gc.ca/apps/pdc/index_E.asp?oqYEAR=2013-2014&oqQUARTER=4&oqCONTRACT_ID=43649"
+  #
+  # Returns a Hash containing the contract data.
+  #
+  #   report = Scrapers::Report.new("pc", "http://www.pc.gc.ca/apps/pdc/index_E.asp?oqYEAR=2013-2014&oqQuarter=4"
+  #   Scrapers::Xyz::Scraper.new(report, ScraperNotifier.new).scrape_contract("http://www.pc.gc.ca/apps/pdc/index_E.asp?oqYEAR=2013-2014&oqQUARTER=4&oqCONTRACT_ID=43649")
+  #   #=>
+  #     { vendor_name: "KONE INC.",
+  #       reference_number: "45340584",
+  #       effective_date: <#Date>,
+  #       contract_period: "2014/03/31 to 2015/03/31",
+  #       url: "http://www.pc.gc.ca/apps/pdc/index_E.asp?oqYEAR=2013-2014&oqQUARTER=4&oqCONTRACT_ID=43649"
+  #       value: 17687,
+  #       description: "665 Other equipment (specify)",
+  #       comments: "Elevator Maintenance"
+  #     }
+  #
+  def scrape_contract(url)
+    mappings = [
+      Scrapers::TableMapping.new(:vendor_name),
+      Scrapers::TableMapping.new(:reference_number),
+      Scrapers::TableMapping.new(:raw_contract_period, "contract period"),
+      Scrapers::TableMapping.new(:effective_date, "contract date"),
+      Scrapers::TableMapping.new(:value, "contract value"),
+      Scrapers::TableMapping.new(:description),
+      Scrapers::TableMapping.new(:comments)
+    ]
+    Scrapers::TableExtractor.new(url, mappings).extract
+  end
+
+  # Returns the urls for the contract pages available in the #report.
+  #
+  #   Scrapers::Xyz::Scraper.new(report).contract_urls
+  #   #=> ["http://www.pc.gc.ca/disclosure/contracts/123", ...]
+  def contract_urls
+    page = Nokogiri::HTML(open(report.url))
+    page.xpath('//table//caption[contains(., "Contracts over $10,000")]//..//td/a').map {|a_tag| "#{BASE_URL}#{a_tag['href']}" }
+  end
+
+  # Scrape the main Reports page for the agency and returns all the report
+  # that the agency has contract data for. Newest report link first.
+  #
+  # Returns an array of Scrapers::Report objects.
+  def self.reports
+    page = Nokogiri::HTML(open("#{BASE_URL}/eng/transparency/disclosure/contracts/index.php?action=index&long_index=1"))
+    links = page.xpath('//p[contains(., "Please select a report")]//..//li//a/@href')
+    links.map {|href| Scrapers::Report.new("wd", "#{BASE_URL}#{href.text}") }
+  end
+end
+
---
+++ test/lib/scrapers/nrc/scraper_test.rb
@@ -0,0 +1,48 @@
+require "test_helper"
+describe Scrapers::Nrc::Scraper do
+
+  describe "#scrape_contract" do
+    it "should parse the data from a contract page" do
+      VCR.use_cassette('nrc_scrape_contracts', record: :new_episodes) do
+        report = mock("report")
+        notifier = mock("notifier")
+        scraper = Scrapers::Nrc::Scraper.new(report, notifier)
+        contract = scraper.scrape_contract("http://www.nrc-cnrc.gc.ca/eng/transparency/disclosure/contracts/index.php?action=details&id=35804")
+
+        contract[:url].must_equal "http://www.nrc-cnrc.gc.ca/eng/transparency/disclosure/contracts/index.php?action=details&id=35804"
+        contract[:vendor_name].must_equal "F.D. MAINTENANCE 2011 INC."
+        contract[:reference_number].must_equal "731313"
+        contract[:raw_contract_period].must_equal "2010-04-01 to 2015-05-31"
+        contract[:effective_date].must_equal Date.parse("2010-04-01")
+        contract[:value].must_equal 909881
+        contract[:description].must_equal "811 CONTRACTED BUILDING CLEANING"
+        contract[:comments].must_equal "This contract includes one or more amendments.The Original Contract Value was: $ 151,442.93This contract was competitively sourced.This contract is a multi-year contract."
+      end
+    end
+  end
+
+  describe "#contract_urls" do
+    it "should return the urls for all contract pages in the report" do
+      VCR.use_cassette('nrc_count_contracts', record: :new_episodes) do
+        report = Scrapers::Report.new('nrc', 'http://www.nrc-cnrc.gc.ca/eng/transparency/disclosure/contracts/index.php?action=quarterly&fy=2015-2016&fq=Q1&lang=eng')
+        notifier = ScraperNotifier.new
+        scraper = Scrapers::Nrc::Scraper.new(report, notifier)
+        scraper.contract_urls.length.must_equal 561
+        scraper.contract_urls.first.must_equal "http://www.nrc-cnrc.gc.ca/eng/transparency/disclosure/contracts/index.php?action=details&id=35804"
+      end
+    end
+  end
+
+  describe ".reports" do
+    it "should return all available reports for the agency" do
+      VCR.use_cassette('nrc_reports', record: :new_episodes) do
+        reports = Scrapers::Nrc::Scraper.reports
+        (reports.length > 44).must_equal true
+        reports.last.must_be_instance_of(Scrapers::Report)
+        reports.last.url.must_equal "http://www.nrc-cnrc.gc.ca/eng/transparency/disclosure/contracts/index.php?action=quarterly&fy=2004-2005&fq=Q1&lang=eng"
+
+      end
+    end
+  end
+end
+
