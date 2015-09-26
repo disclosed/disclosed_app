134d4c1a41eec3fbb1b9280795cabd9f37f58fba
Monica Olinescu <monica.olinescu@gmail.com>
2015-09-26 14:34:18 +0200

    ContractUrlExtractor handles a tags with empty hrefs

--- lib/scrapers/contract_url_extractor.rb
+++ lib/scrapers/contract_url_extractor.rb
@@ -6,7 +6,9 @@ class Scrapers::ContractUrlExtractor
     page = Nokogiri::HTML(open(report_url))
     a_tags = page.xpath("//table[contains(., '" + th_label + "')]//a")
     @urls = a_tags.map do |a_tag|
+      next if a_tag['href'].nil?
       full_url(page, report_url, a_tag['href'])
     end
+    @urls.compact!
   end
 end
