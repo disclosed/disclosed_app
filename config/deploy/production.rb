4b5f4a624e53cacf09941e5dad315d939c95edeb
Monica Olinescu <monica.olinescu@gmail.com>
2015-09-27 23:14:14 +0200

    Ability to tail production logs

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -61,6 +61,15 @@ namespace :contracts do
   end
 end
 
+namespace :logs do
+  desc "tail rails logs" 
+  task :tail_rails do
+    on roles(:app) do
+      execute "tail -f #{shared_path}/log/#{fetch(:rails_env)}.log"
+    end
+  end
+end
+
 namespace :deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision do
